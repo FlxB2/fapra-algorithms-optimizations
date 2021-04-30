a96c6c4d75be926b0927a8bb234615179cffac02
Maximilian Reichel <reichemn@icloud.com>
2021-04-30 17:26:39 +0200

    first try

--- osm-tasks/src/main.rs
+++ osm-tasks/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    use osmpbf::{ElementReader, Element};
+
+    let reader = ElementReader::from_path("./monaco-latest.osm.pbf").expect("failed");
+    let mut ways = 0_u64;
+
+// Increment the counter by one for each way.
+    reader.for_each(|element| {
+        if let Element::Way(_) = element {
+            ways += 1;
+        }
+    }).expect("failed5");
 }
