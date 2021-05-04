5bf077a95434ff450c79678bd862e02a02203267
Maximilian Reichel <reichemn@icloud.com>
2021-05-04 09:57:38 +0200

    fixed coordinate mapping and implemented generation of geojson json files

--- osm-tasks/src/main.rs
+++ osm-tasks/src/main.rs
@@ -3,9 +3,12 @@ use std::time::Instant;
 
 use osmpbf::{Element, ElementReader};
 use rayon::prelude::*;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     println!("Hello, world!");
+    let main_start_time = Instant::now();
 
     //let mut coastlines_map = HashMap::new();
     //let mut ways_to_nodes_map = HashMap::new();
@@ -13,56 +16,53 @@ fn main() {
     let mut number_merged_nodes = 0_u64;
     let mut merged_ways: LinkedList<Coastline> = LinkedList::new();
 
-    //let reader = ElementReader::from_path("./monaco-latest.osm.pbf").expect("failed");
+    let reader = ElementReader::from_path("./monaco-latest.osm.pbf").expect("failed");
     //let reader = ElementReader::from_path("./iceland-coastlines.osm.pbf").expect("failed");
     //let reader = ElementReader::from_path("./sa-coastlines.osm.pbf").expect("failed");
-    let reader = ElementReader::from_path("./planet-coastlines.osm.pbf").expect("failed");
+    //let reader = ElementReader::from_path("./planet-coastlines.osm.pbf").expect("failed");
     // let mut ways = 0_u64;
     let mut coastlines = 0_u64;
     let (coastlines_map_wrapped, ways_to_nodes_map_wrapped, node_coordinates_map_wrapped) = reader.par_map_reduce(
         |element| {
-            if let Element::Way(way) = element {
-                for (key, value) in way.tags() {
-                    if let ("natural", "coastline") = (key, value) {
-                        let nodes: Vec<i64> = way.refs().collect();
-                        if nodes.len() <= 1 {
-                            // Way with a single node does not give as any information -> discard
-                            break;
-                        }
-                        if let Some(first_node) = nodes.first() {
-                            if let Some(last_node) = nodes.last() {
-                                if *first_node == *last_node {
-                                    // already closed polygon
-                                   /* let mut llist = LinkedList::new();
-                                    llist.push_back(way.id());
-                                    merged_ways.push_back(Coastline { nodes, ways: llist });
-                                    number_merged_nodes += 1;*/
-                                    // Todo: handle these ways
-                                    println!("Discarded polygon. Fix this to handle this case correctly");
-                                    break;
-                                } else {
-                                    return (MapOrEntry::Entries(vec![(*first_node, WayNodePair { way: way.id(), node: *last_node }),
-                                                                     (*last_node, WayNodePair { way: way.id(), node: *first_node })]),
-                                            MapOrEntry::Entries(vec![(way.id(), nodes )]),
-                                    MapOrEntry::Entries(vec![]));
+            match element {
+                Element::Way(way) => {
+                    if let Some(_) = way.tags().find(|(k,v)| *k == "natural" && *v == "coastline"){
+                            let nodes: Vec<i64> = way.refs().collect();
+                            if nodes.len() <= 1 {
+                                // Way with a single node does not give as any information -> discard
+
+                            }else if let Some(first_node) = nodes.first() {
+                                if let Some(last_node) = nodes.last() {
+                                    if *first_node == *last_node {
+                                        // already closed polygon
+                                        /* let mut llist = LinkedList::new();
+                                         llist.push_back(way.id());
+                                         merged_ways.push_back(Coastline { nodes, ways: llist });
+                                         number_merged_nodes += 1;*/
+                                        // Todo: handle these ways
+                                        println!("Discarded polygon. Fix this to handle this case correctly");
+                                    } else {
+                                        return (MapOrEntry::Entries(vec![(*first_node, WayNodePair { way: way.id(), node: *last_node }),
+                                                                         (*last_node, WayNodePair { way: way.id(), node: *first_node })]),
+                                                MapOrEntry::Entries(vec![(way.id(), nodes)]),
+                                                MapOrEntry::Entries(vec![]));
+                                    }
                                 }
                             }
                         }
-                        break;
-                        // println!("key: {}, value: {}", key, value);
-                    }
+
                 }
-            }else {
-                if let Element::Node(node) = element {
-                    return (MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![(node.id(),(node.lat(), node.lon()))]));
-                }
+                Element::Node(node) => return (MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![(node.id(), (node.lon(), node.lat()))])),
+                Element::DenseNode(node) => return (MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![(node.id(), (node.lon(), node.lat()))])),
+                _ => {}
             }
+
             (MapOrEntry::Entries(vec![]), MapOrEntry::Entries(vec![]), MapOrEntry::<(f64, f64)>::Entries(vec![]))
         },
         || (MapOrEntry::Map(HashMap::new()), MapOrEntry::Map(HashMap::new()), MapOrEntry::Map(HashMap::new())),      // Zero is the identity value for addition
-        |(a1,a2, a3),(b1, b2, b3)|{
+        |(a1, a2, a3), (b1, b2, b3)| {
             (MapOrEntry::<WayNodePair>::combine(a1, b1), MapOrEntry::<Vec<i64>>::combine(a2, b2), MapOrEntry::<(f64, f64)>::combine(a3, b3))
-        }
+        },
     ).expect("fail");
     let mut coastlines_map = coastlines_map_wrapped.unwrap_map();
     let ways_to_nodes_map = ways_to_nodes_map_wrapped.unwrap_map();
@@ -123,7 +123,7 @@ fn main() {
         let first_pair = pair_list.first().expect("Pair list empty for node");
         let mut list = LinkedList::new();
         list.push_back(first_pair.way);
-        let mut current_coastline = Coastline { nodes: vec![], ways: list, top_node: *first_node_top};
+        let mut current_coastline = Coastline { nodes: vec![], ways: list, top_node: *first_node_top };
         let mut reached_unconnected_node_top = false;
         let mut reached_unconnected_node_bottom = false;
         let mut bottom_node_coastline = first_pair.node;
@@ -235,16 +235,16 @@ fn main() {
     merged_ways.iter().for_each(|e| {
         if e.ways.len() > 1 {
             println!("Coastline from way {} to {} merged out of {} ways", e.ways.front().unwrap(), e.ways.back().unwrap(), e.ways.len())
-        }else {number_one_way_coastlines += 1;  }
+        } else { number_one_way_coastlines += 1; }
     });
     println!("+ {} coastlines merged out of a single way", number_one_way_coastlines);
 
 
-    merged_ways = merged_ways.into_iter().par_bridge().map(|mut coastline|{
+    let polygons: Vec<Vec<(f64, f64)>> = merged_ways.into_iter().par_bridge().map(|mut coastline| {
         coastline.nodes.reserve(1000);
         let mut current_top_node = coastline.top_node;
         let mut coastline_nodes = coastline.nodes;
-        coastline.ways.iter().for_each(|way|{
+        coastline.ways.iter().for_each(|way| {
             let way_copy = *way;
             if let Some(nodes) = ways_to_nodes_map.get(&way_copy) {
                 if !coastline_nodes.is_empty() {
@@ -257,92 +257,114 @@ fn main() {
                     coastline_nodes.append(&mut nodes.first().unwrap().clone());
                 } else {
                     // nodes are reversed
-                    let mut reversed_other_nodes =  nodes.first().unwrap().clone();
+                    let mut reversed_other_nodes = nodes.first().unwrap().clone();
                     reversed_other_nodes.reverse();
                     coastline_nodes.append(&mut reversed_other_nodes);
                 }
                 current_top_node = *coastline_nodes.last().unwrap();
-            }else{
+            } else {
                 //Should not happen
                 println!("Could not resolve node list for way: {}", way_copy)
             }
         });
         coastline.nodes = coastline_nodes;
-        return coastline
-    }).collect();
-
-    let ways_to_coords_map : HashMap<i64, Vec<(f64, f64)>> = ways_to_nodes_map.into_iter().par_bridge().map(|(k,v)|{
-        let mut coords : Vec<(f64,f64)> = Vec::with_capacity(v.len());
-        v.iter().for_each(|nodeId|{
-            if let Some(coord) = node_coordinate_map.get(nodeId.first().unwrap()){
+        let mut coords: Vec<(f64, f64)> = Vec::with_capacity(coastline.nodes.len());
+        coastline.nodes.iter().for_each(|node_id| {
+            if let Some(coord) = node_coordinate_map.get(node_id) {
                 coords.push(*coord.first().unwrap());
             }
         });
-        return (k, coords);
+        return coords;
     }).collect();
 
-    merged_ways.iter().par_bridge().for_each(|e|{
-        println!("Coastline out of {} ways and {} nodes", e.ways.len(), e.nodes.len());
-    })
+    let count_all_polygons = polygons.len();
+
+    // filter for closed polygons since the others are more a line than a real polygon
+    let mut closed_polygons: Vec<Vec<(f64, f64)>>= polygons.into_iter().par_bridge().filter(|e| e.len()>1 && e.first().unwrap() == e.last().unwrap()).collect();
+    closed_polygons.sort_by(|a,b| b.len().cmp(&a.len()));
+    closed_polygons.iter().take(15).par_bridge().for_each(|e| {
+        println!("Polygon out of {} coords", e.len());
+    });
+    closed_polygons.iter().take(10).zip(1..10).par_bridge().for_each(|(coords, idx)| {
+        let geojson_string = polygon_geojson_string(coords);
+        let mut file = File::create(format!("polygon_{}.json", idx)).expect("could not open file");
+        file.write_all(geojson_string.as_bytes()).expect("could not write to file");
+    });
+
+    println!("{} out of {} polygons are closed", closed_polygons.len(), count_all_polygons);
+
+    println!("total time: {} sec", main_start_time.elapsed().as_secs());
 }
 
+fn polygon_geojson_string(coords: &Vec<(f64, f64)>) -> String{
+    let coords_string = format!("{:?}", coords).replace("(", "[").replace(")", "]");
+    format!("{{
+      \"type\": \"Feature\",
+      \"properties\": {{}},
+      \"geometry\": {{
+        \"type\": \"Polygon\",
+        \"coordinates\": [
+            {}
+        ]
+      }}
+     }}", coords_string)
+}
 
 struct Coastline {
     nodes: Vec<i64>,
     ways: LinkedList<i64>,
-    top_node: i64
+    top_node: i64,
 }
 
 #[derive(Copy, Clone)]
 struct WayNodePair {
     way: i64,
-    node: i64
+    node: i64,
 }
 
-enum MapOrEntry<T>{
+enum MapOrEntry<T> {
     Map(HashMap<i64, Vec<T>>),
-    Entries(Vec<(i64, T)>)
+    Entries(Vec<(i64, T)>),
 }
 
-    fn combineT<T>(a: MapOrEntry<T>, b: MapOrEntry<T>) -> MapOrEntry<T>{
-        match (a, b) {
-            (MapOrEntry::Map(mut map_a), MapOrEntry::Map(map_b))=>{
-                map_a.extend(map_b);
-                return MapOrEntry::Map(map_a);
-            }
-            (MapOrEntry::Map(mut map), MapOrEntry::Entries(entries)) | (MapOrEntry::Entries(entries), MapOrEntry::Map(mut map)) =>{
-                entries.into_iter().for_each(|(key, entry)|{
-                    if map.contains_key(&key) {
-                        map.entry(key).and_modify(|e: &mut Vec<T>| { e.push(entry)});
-                    }else {
-                        map.insert(key,vec![entry]);
-                    }
-                });
-                return MapOrEntry::Map(map);
-            }
-            (MapOrEntry::Entries(entries_a), MapOrEntry::Entries(entries_b)) => {
-                let mut map = HashMap::new();
-                entries_a.into_iter().for_each(|(key, entry)|{
-                    if map.contains_key(&key){
-                        map.entry(key).and_modify(|e: &mut Vec<T>| e.push(entry));
-                    }else{
-                        map.insert(key,vec![entry]);
-                    }
-                });
-                entries_b.into_iter().for_each(|(key, entry)|{
-                    if map.contains_key(&key){
-                        map.entry(key).and_modify(|e: &mut Vec<T>| { e.push(entry)});
-                    }else{
-                        map.insert(key,vec![entry]);
-                    }
-
-                });
-                return MapOrEntry::Map(map);
-            }
+fn combineT<T>(a: MapOrEntry<T>, b: MapOrEntry<T>) -> MapOrEntry<T> {
+    match (a, b) {
+        (MapOrEntry::Map(mut map_a), MapOrEntry::Map(map_b)) => {
+            map_a.extend(map_b);
+            return MapOrEntry::Map(map_a);
+        }
+        (MapOrEntry::Map(mut map), MapOrEntry::Entries(entries)) | (MapOrEntry::Entries(entries), MapOrEntry::Map(mut map)) => {
+            entries.into_iter().for_each(|(key, entry)| {
+                if map.contains_key(&key) {
+                    map.entry(key).and_modify(|e: &mut Vec<T>| { e.push(entry) });
+                } else {
+                    map.insert(key, vec![entry]);
+                }
+            });
+            return MapOrEntry::Map(map);
+        }
+        (MapOrEntry::Entries(entries_a), MapOrEntry::Entries(entries_b)) => {
+            let mut map = HashMap::new();
+            entries_a.into_iter().for_each(|(key, entry)| {
+                if map.contains_key(&key) {
+                    map.entry(key).and_modify(|e: &mut Vec<T>| e.push(entry));
+                } else {
+                    map.insert(key, vec![entry]);
+                }
+            });
+            entries_b.into_iter().for_each(|(key, entry)| {
+                if map.contains_key(&key) {
+                    map.entry(key).and_modify(|e: &mut Vec<T>| { e.push(entry) });
+                } else {
+                    map.insert(key, vec![entry]);
+                }
+            });
+            return MapOrEntry::Map(map);
         }
     }
+}
 
-impl <T> MapOrEntry<T>{
+impl<T> MapOrEntry<T> {
     fn unwrap_map(self) -> HashMap<i64, Vec<T>> {
         match self {
             MapOrEntry::Map(map) => map,
@@ -350,7 +372,7 @@ impl <T> MapOrEntry<T>{
         }
     }
 
-    fn combine<T1>(a: MapOrEntry<T1>, b: MapOrEntry<T1>) -> MapOrEntry<T1>{
+    fn combine<T1>(a: MapOrEntry<T1>, b: MapOrEntry<T1>) -> MapOrEntry<T1> {
         return combineT(a, b);
     }
 }
